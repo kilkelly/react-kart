a52a91231b8c8d681cc697ef238b4b3dd2939b76
Frank Kilkelly <frank.kilkelly@gmail.com>
2016-05-05 13:20:30 +0100

    Fixed GitHub Star link in the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,7 +11,7 @@ const Footer = React.createClass({
 
 		return (
 			<div id={styles.wrapper}>				
-				<iframe src="https://ghbtns.com/github-btn.html?user=kilkelly&repo=reactkart&type=star&count=true" frameBorder="0" scrolling="0" width="170px" height="20px"></iframe><br/>
+				<iframe src="https://ghbtns.com/github-btn.html?user=kilkelly&repo=react-kart&type=star&count=true" frameBorder="0" scrolling="0" width="170px" height="20px"></iframe><br/>
 				<a href="https://github.com/kilkelly/react-kart" target="_blank">ReactKart on GitHub</a> by <a href="https://github.com/kilkelly/" target="_blank">Frank Kilkelly</a>
 			</div>			
 		)
